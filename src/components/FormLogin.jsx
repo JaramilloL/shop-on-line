41b5b434232ca2d124375b951cea6e490e221aa7
JLJL <juanluisjaramillolopez@gmail.com>
2024-02-23 16:22:16 -0600

    implementacion de la libreria de react-hook-form para la captura de datos

--- src/components/FormLogin.jsx
+++ src/components/FormLogin.jsx
@@ -1,8 +1,23 @@
 //este componente contiene el formulario de login del usuario
+//implementaremos la libreria de react-hook-form para la data de cada formulario
+import { useForm } from 'react-hook-form'
 
 const FormLogin = () => {
+  const { handleSubmit, register, reset, formState: { errors } } = useForm();
+
+    //creamos la funcion de envio de datos del formulario
+    const onSubmit =  handleSubmit((data)=>{
+        try {
+            console.log(data);
+            reset();
+        } catch (error) {
+            console.log(error)
+        }
+        
+    })
+
   return (
-    <form className="container w-50 mt-5">
+    <form className="container w-50 mt-5" onSubmit={ onSubmit }>
       <div className="form-floating mb-3">
         <input
           type="email"
@@ -10,8 +25,17 @@ const FormLogin = () => {
           id="floatingInput"
           placeholder="name@example.com"
           name="email"
+          {
+            ...register('email', {
+              required: {
+                value: true,
+                message:'email is required'
+              }
+            })
+          }
         />
         <label htmlFor="floatingInput">Email address</label>
+        <p className="text-danger text-center">{ errors?.email?.message }</p>
       </div>
       <div className="form-floating">
         <input
@@ -20,8 +44,17 @@ const FormLogin = () => {
           id="floatingPassword"
           placeholder="Password"
           name="password"
+          {
+            ...register('password', {
+              required: {
+                value: true,
+                message:'password is required'
+              }
+            })
+          }
         />
         <label htmlFor="floatingPassword">Password</label>
+        <p className="text-danger text-center">{ errors?.email?.message }</p>
       </div>
       <div className="d-flex justify-content-center align-content-center align-items-center">
         <button className="btn btn-success mt-3">LogIn</button>
--- src/components/FormRegister.jsx
+++ src/components/FormRegister.jsx
@@ -1,8 +1,22 @@
 //este componente contiene el formulario de registro del usuario
+//usaremos el hook de useForm que viene de react-hppk-form para la manipulacion de los datos del formulario
+import { useForm } from 'react-hook-form'
 
 const FormRegister = () => {
+    const { handleSubmit, register, reset, formState: { errors } } = useForm();
+
+    //creamos la funcion de envio de datos del formulario
+    const onSubmit =  handleSubmit((data)=>{
+        try {
+            console.log(data);
+            reset();
+        } catch (error) {
+            console.log(error)
+        }
+        
+    })
   return (
-    <form className="container w-50 mt-5">
+    <form className="container w-50 mt-5" onSubmit={ onSubmit }>
     <div className="form-floating mb-3">
         <input
           type="text"
@@ -10,8 +24,25 @@ const FormRegister = () => {
           id="floatingName"
           placeholder="name"
           name="userName"
+          {
+            ...register('userName', {
+              required: {
+                value: true,
+                message:'name is required'
+              },
+              minLength: {
+                value: 4,
+                message: 'more than 4 characters required'    
+              },
+              maxLength: {
+                value: 55,
+                message: 'too many characters required'
+              }
+            })
+          }
         />
         <label htmlFor="floatingName">User Name</label>
+        <p className="text-danger text-center">{ errors?.userName?.message }</p>
       </div>
       <div className="form-floating mb-3">
         <input
@@ -20,8 +51,17 @@ const FormRegister = () => {
           id="floatingInput"
           placeholder="name@example.com"
           name="email"
+          {
+            ...register('email', {
+              required: {
+                value: true,
+                message:'email is required'
+              }
+            })
+          }
         />
         <label htmlFor="floatingInput">Email address</label>
+        <p className="text-danger text-center">{ errors?.email?.message }</p>
       </div>
       <div className="form-floating">
         <input
@@ -30,8 +70,17 @@ const FormRegister = () => {
           id="floatingPassword"
           placeholder="Password"
           name="password"
+          {
+            ...register('password', {
+              required: {
+                value: true,
+                message:'password is required'
+              }
+            })
+          }
         />
         <label htmlFor="floatingPassword">Password</label>
+        <p className="text-danger text-center">{ errors?.password?.message }</p>
       </div>
       <div className="d-flex justify-content-center align-content-center align-items-center">
         <button className="btn btn-success m-3">Register</button>
