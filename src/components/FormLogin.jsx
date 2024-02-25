8504dfa72729ce93145f62d85a8fa57abd184764
JLJL <juanluisjaramillolopez@gmail.com>
2024-02-25 11:35:28 -0600

    configuraciones del estado global y permisos

--- src/components/FormLogin.jsx
+++ src/components/FormLogin.jsx
@@ -1,34 +1,60 @@
 //este componente contiene el formulario de login del usuario
 //implementaremos la libreria de react-hook-form para la data de cada formulario
-import { useContext } from 'react';
-import { useForm } from 'react-hook-form'
-import { UserContext } from '../context/UserContext';
-import { useNavigate } from 'react-router-dom';
+import { useContext } from "react";
+import { useForm } from "react-hook-form";
+import { UserContext } from "../context/UserContext";
+import { Link, useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const FormLogin = () => {
   //llamamos al contexto para actualizar el envio de login a dashboard
-  const { loginUser } = useContext(UserContext);
-  
+  const { loginUser, loginAccess, signInWithGoogle, resetPassword } =
+    useContext(UserContext);
+
   //?usamos la navegacion para la redireccion de pagina
   const userNavigate = useNavigate();
+  const {
+    handleSubmit,
+    register,
+    reset,
+    formState: { errors },
+    getValues,
+  } = useForm();
 
-  const { handleSubmit, register, reset, formState: { errors } } = useForm();
+  //creamos la funcion de envio de datos del formulario
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      console.log(data);
+      loginUser(data.email, data.password);
+      loginAccess();
+      userNavigate("/");
+      reset();
+    } catch (error) {
+      console.log(error);
+    }
+  });
+  //creamos la funcio para el inicio de secion de google
+  const signInGoogle = async () => {
+    try {
+      await signInWithGoogle();
+      loginAccess();
+      userNavigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    //creamos la funcion de envio de datos del formulario
-    const onSubmit =  handleSubmit(async(data)=>{
-        try {
-            console.log(data);
-            loginUser(data.email, data.password);
-            reset();
-            userNavigate('/')
-        } catch (error) {
-            console.log(error)
-        }
-        
-    })
+  const resetP = async () => {
+    const emailValue = getValues("email");
+    if (!emailValue) {
+      toast.error("enter yout email");
+    }
+    await resetPassword(emailValue);
+  };
 
   return (
-    <form className="container w-50 mt-5" onSubmit={ onSubmit }>
+    <form className="container w-50 mt-5" onSubmit={onSubmit}>
       <div className="form-floating mb-3">
         <input
           type="email"
@@ -36,17 +62,15 @@ const FormLogin = () => {
           id="floatingInput"
           placeholder="name@example.com"
           name="email"
-          {
-            ...register('email', {
-              required: {
-                value: true,
-                message:'email is required'
-              }
-            })
-          }
+          {...register("email", {
+            required: {
+              value: true,
+              message: "email is required",
+            },
+          })}
         />
         <label htmlFor="floatingInput">Email address</label>
-        <p className="text-danger text-center">{ errors?.email?.message }</p>
+        <p className="text-danger text-center">{errors?.email?.message}</p>
       </div>
       <div className="form-floating">
         <input
@@ -55,21 +79,34 @@ const FormLogin = () => {
           id="floatingPassword"
           placeholder="Password"
           name="password"
-          {
-            ...register('password', {
-              required: {
-                value: true,
-                message:'password is required'
-              }
-            })
-          }
+          {...register("password", {
+            required: {
+              value: true,
+              message: "password is required",
+            },
+          })}
         />
         <label htmlFor="floatingPassword">Password</label>
-        <p className="text-danger text-center">{ errors?.email?.message }</p>
+        <p className="text-danger text-center">{errors?.email?.message}</p>
+
+        <div className="d-flex justify-content-center align-content-center align-align-items-center">
+          <Link className="text-primary mt-3 text-center" onClick={resetP}>
+            Forgot your password?
+          </Link>
+        </div>
       </div>
       <div className="d-flex justify-content-center align-content-center align-items-center">
-        <button className="btn btn-success mt-3">LogIn</button>
+        <button className="btn btn-success mt-3 w-25">LogIn</button>
       </div>
+      <div className="d-flex justify-content-center align-content-center align-items-center">
+        <button
+          className="btn border-danger text-danger mt-3 w-25"
+          onClick={signInGoogle}
+        >
+          Google
+        </button>
+      </div>
+      <ToastContainer />
     </form>
   );
 };
--- src/context/StateContext.jsx
+++ src/context/StateContext.jsx
@@ -1,40 +1,82 @@
 //este componente manejara todas las funciones y compoartira la informacion creada a el contexto de toda la aplicacion
-//usamos proptypes para a validacion de los datos de entradas 
-import PropTypes from 'prop-types'
-import { useReducer } from "react"
-import ReducerContext from "./ReducerContext"
-import { UserContext } from '../context/UserContext'
-import { auth } from '../firebase/firebaseConfig'
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
+//usamos proptypes para a validacion de los datos de entradas
+import PropTypes from "prop-types";
+import { useEffect, useReducer, useState } from "react";
+import ReducerContext from "./ReducerContext";
+import { UserContext } from "../context/UserContext";
+import { auth } from "../firebase/firebaseConfig";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  GoogleAuthProvider,
+  signInWithPopup,
+  sendPasswordResetEmail,
+  onAuthStateChanged,
+} from "firebase/auth";
 
-const StateContext = ( {children} ) => {
-    //esto sera el estado inicial de la app
-    const initialState = {
-        isAutenticated : false,
-        user: null
-    }
-    //vamos a crear una funcion para registrar un usuario en firebase
-    const registerUser = (email, password) => createUserWithEmailAndPassword(auth, email, password);
+const StateContext = ({ children }) => {
+  //esto sera el estado inicial de la app
+  const initialState = {
+    isAutenticated: false,
+    user: null,
+  };
+  //vamos a crear una funcion para registrar un usuario en firebase
+  const registerUser = (email, password) =>
+    createUserWithEmailAndPassword(auth, email, password);
 
-    //vamos a crear la funcion de login en el usuario
-    const loginUser = async(email, password) => await signInWithEmailAndPassword(auth, email, password);
+  //vamos a crear la funcion de login en el usuario
+  const loginUser = async (email, password) =>
+    await signInWithEmailAndPassword(auth, email, password);
 
-    //usamos el reducer para traer la validacion del switch que existe en el ReducerContext
-    const [state, dispatch] = useReducer(ReducerContext, initialState)
+  //creamos loa accesos a la pagina de dashboard
+  const loginAccess = () => dispatch({ type: "login" });
+  const logoutAccess = () => dispatch({ type: "logout" });
+
+  const logout = () => signOut();
+
+  //creamos el inicio de secion con google
+  const signInWithGoogle = () => {
+    const google = new GoogleAuthProvider();
+    return signInWithPopup(auth, google);
+  };
+
+  //creamos funcion para un reset de password
+  const resetPassword = (email) => sendPasswordResetEmail(auth, email);
+  const [user, setUser] = useState()
+
+   //usamos un useeffect para ver el tipo de logeo del usuario es decir si fue mediente google o por correo y password
+   useEffect(()=>{
+    onAuthStateChanged((stateAuth) => {
+      console.log(stateAuth)
+      setUser(stateAuth)
+    })
+  })
+
+  //usamos el reducer para traer la validacion del switch que existe en el ReducerContext
+  const [state, dispatch] = useReducer(ReducerContext, initialState);
   return (
-    <UserContext.Provider value={{
+    <UserContext.Provider
+      value={{
         state,
         dispatch,
         registerUser,
-        loginUser
-    }}>
-        {children}
+        loginUser,
+        loginAccess,
+        logoutAccess,
+        logout,
+        signInWithGoogle,
+        resetPassword,
+        user
+      }}
+    >
+      {children}
     </UserContext.Provider>
-  )
-}
+  );
+};
 
-export default StateContext
+export default StateContext;
 
 StateContext.propTypes = {
-    children: PropTypes.any
-}
+  children: PropTypes.any,
+};
