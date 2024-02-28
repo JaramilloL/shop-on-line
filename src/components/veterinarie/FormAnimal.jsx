7e3c9ecd734c03a67ea6fcdc1d20693301b7d2b3
JLJL <juanluisjaramillolopez@gmail.com>
2024-02-28 14:40:04 -0600

    agregaciones de los datos de input a react-hook-form para su captura

--- src/components/veterinarie/FormAnimal.jsx
+++ src/components/veterinarie/FormAnimal.jsx
@@ -1,8 +1,78 @@
+//usamos el hook de react-hook-form para guardar los datos del formulario y despues trabajarlo
+import { useForm } from "react-hook-form";
+//vamos a usar un id unico para cada una de ls concultas
+import { v4 as uuidv4 } from "uuid";
+//importamos desde firebase las colleccones y el documento para el envio de daos
+import { collection, addDoc } from "firebase/firestore";
+import { bd } from "../../firebase/firebaseConfig";
+
 const FormAnimal = () => {
+  //desestructuramos las funciones que vienen desde react-hook-form
+  const {
+    handleSubmit,
+    reset,
+    register,
+    formState: { errors },
+    getValues,
+  } = useForm();
+
+  //creamos la funcion que enviara los datos
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+  });
+
+  const getVeters = async () => {
+    const nameAnimal = getValues("nameAnimal");
+    const typeAnimal = getValues("typeAnimal");
+    const breed = getValues("breed");
+    const age = getValues("age");
+    const gender = getValues("gender");
+    const color = getValues("color");
+    const kilograms = getValues("kilograms");
+    const vaccibes = getValues("vaccibes");
+    const allergies = getValues("allergies");
+    const reason = getValues("reason");
+    const description = getValues("description");
+    const id = uuidv4();
+    try {
+      console.log(
+        nameAnimal,
+        typeAnimal,
+        breed,
+        age,
+        gender,
+        color,
+        kilograms,
+        vaccibes,
+        allergies,
+        reason,
+        description,
+        id
+      );
+      const getData = await addDoc(collection(bd, "veterinaria"), {
+        id: id,
+        nameAnimal: nameAnimal,
+        typeAnimal: typeAnimal,
+        breed: breed,
+        age: age,
+        gender: gender,
+        color: color,
+        kilograms: kilograms,
+        vaccibes: vaccibes,
+        allergies: allergies,
+        reason: reason,
+        description: description,
+      });
+      console.log(getData.id);
+      reset();
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <div>
       <h3>Form Animal</h3>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="mb-3">
           <label htmlFor="formName" className="form-label">
             Name:
@@ -13,7 +83,24 @@ const FormAnimal = () => {
             id="formName"
             placeholder="name..."
             name="nameAnimal"
+            {...register("nameAnimal", {
+              required: {
+                value: true,
+                message: "Please enter a name",
+              },
+              minLength: {
+                value: 5,
+                message: "Please enter more than 5 characters",
+              },
+              maxLength: {
+                value: 25,
+                message: "Please enter less than 25 characters",
+              },
+            })}
           />
+          <p className="text-center text-danger">
+            {errors?.nameAnimal?.message}
+          </p>
         </div>
         <div className="mb-3">
           <label className="form-label">Select specie:</label>
@@ -21,16 +108,24 @@ const FormAnimal = () => {
             className="form-select"
             aria-label="Default select example"
             name="typeAnimal"
+            {...register("typeAnimal", {
+              required: {
+                value: true,
+                message: "Please enter a typeAnimal",
+              },
+            })}
           >
-            <option defaultValue={"select"}>....</option>
             <option value="Dog">Dog</option>
             <option value="Cat">Cat</option>
             <option value="Bird">Bird</option>
           </select>
+          <p className="text-center text-danger">
+            {errors?.typeAnimal?.message}
+          </p>
         </div>
         <div className="mb-3">
           <label htmlFor="formBread" className="form-label">
-            Breed:
+            Breeds:
           </label>
           <input
             type="text"
@@ -38,7 +133,22 @@ const FormAnimal = () => {
             id="formBread"
             placeholder="Raza"
             name="breed"
+            {...register("breed", {
+              required: {
+                value: true,
+                message: "Please enter a breed",
+              },
+              minLength: {
+                value: 3,
+                message: "Please enter more than 3 characters",
+              },
+              maxLength: {
+                value: 25,
+                message: "Please enter less than 25 characters",
+              },
+            })}
           />
+          <p className="text-center text-danger">{errors?.breed?.message}</p>
         </div>
         <div className="mb-3">
           <label
@@ -48,12 +158,19 @@ const FormAnimal = () => {
             Age:
           </label>
           <input
-            type="text"
+            type="number"
             className="form-control"
             id="formAge"
             placeholder="name..."
             name="age"
+            {...register("age", {
+              required: {
+                value: true,
+                message: "Please enter a age",
+              },
+            })}
           />
+          <p className="text-center text-danger">{errors?.age?.message}</p>
         </div>
         <div className="mb-3">
           <label className="form-label">Gender:</label>
@@ -61,12 +178,18 @@ const FormAnimal = () => {
             className="form-select"
             aria-label="Default select example"
             name="gender"
+            {...register("gender", {
+              required: {
+                value: true,
+                message: "Please enter a gender",
+              },
+            })}
           >
-            <option defaultValue={"select"}>....</option>
             <option value="Male">Male</option>
             <option value="Female">Female</option>
             <option value="Stranger">Stranger</option>
           </select>
+          <p className="text-center text-danger">{errors?.gender?.message}</p>
         </div>
         <div className="mb-3">
           <label htmlFor="formColor" className="form-label">
@@ -78,7 +201,14 @@ const FormAnimal = () => {
             id="formColor"
             placeholder="color..."
             name="color"
+            {...register("color", {
+              required: {
+                value: true,
+                message: "Please enter a color",
+              },
+            })}
           />
+          <p className="text-center text-danger">{errors?.color?.message}</p>
         </div>
         <div className="mb-3">
           <label htmlFor="formKl" className="form-label">
@@ -90,12 +220,21 @@ const FormAnimal = () => {
             id="formKl"
             placeholder="kilograms..."
             name="kilograms"
+            {...register("kilograms", {
+              required: {
+                value: true,
+                message: "Please enter a kilograms",
+              },
+            })}
           />
+          <p className="text-center text-danger">
+            {errors?.kilograms?.message}
+          </p>
         </div>
         <div className="mb-3">
           <div className="mb-3">
             <label htmlFor="vacciones" className="form-label">
-              Vaccines:
+              Vaccibes:
             </label>
             <textarea
               type="text"
@@ -103,7 +242,16 @@ const FormAnimal = () => {
               id="vaccibes"
               name="vaccibes"
               placeholder="vaccibes receibed..."
+              {...register("vaccibes", {
+                required: {
+                  value: true,
+                  message: "Please enter a vaccibes",
+                },
+              })}
             />
+            <p className="text-center text-danger">
+              {errors?.vaccibes?.message}
+            </p>
           </div>
 
           <div className="mb-3">
@@ -116,7 +264,16 @@ const FormAnimal = () => {
               id="allergies"
               name="allergies"
               placeholder="allergies..."
+              {...register("allergies", {
+                required: {
+                  value: true,
+                  message: "Please enter a allergies",
+                },
+              })}
             />
+            <p className="text-center text-danger">
+              {errors?.allergies?.message}
+            </p>
           </div>
           <div className="mb-3">
             <label htmlFor="exampleFormControlTextarea1" className="form-label">
@@ -127,7 +284,16 @@ const FormAnimal = () => {
               id="exampleFormControlTextarea1"
               rows="3"
               name="description"
+              {...register("description", {
+                required: {
+                  value: true,
+                  message: "Please enter a description",
+                },
+              })}
             ></textarea>
+            <p className="text-center text-danger">
+              {errors?.description?.message}
+            </p>
           </div>
           <div className="mb-3">
             <label htmlFor="reason" className="form-label">
@@ -140,8 +306,20 @@ const FormAnimal = () => {
               rows="4"
               cols="50"
               placeholder="reason..."
+              {...register("reason", {
+                required: {
+                  value: true,
+                  message: "Please enter a reason",
+                },
+              })}
             ></textarea>
+            <p className="text-center text-danger">{errors?.reason?.message}</p>
           </div>
+        </div>
+        <div className="d-flex justify-content-center align-content-center align-items-center">
+          <button className="btn btn-success" onClick={getVeters}>
+            Accept
+          </button>
         </div>
       </form>
     </div>
--- src/components/veterinarie/FormUser.jsx
+++ src/components/veterinarie/FormUser.jsx
@@ -1,9 +1,130 @@
+//usamos el hook de react-hook-form patra la extraccion de datos del formulario
+import { addDoc, collection } from "firebase/firestore";
+import { useForm } from "react-hook-form";
+import { bd } from "../../firebase/firebaseConfig";
+
 const FormUser = () => {
+  const {
+    handleSubmit,
+    reset,
+    register,
+    formState: { errors },
+    getValues,
+  } = useForm();
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+    reset();
+  });
+
+  const getDataForm = async () => {
+    const username = getValues("username");
+    console.log(username);
+
+    try {
+      const dataResult = await addDoc(collection(bd, "users"), {
+        username: username,
+      });
+      console.log(dataResult.id);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
       <h3>Form User</h3>
-    </div>
-  )
-}
+      <form onSubmit={onSubmit}>
+        <div className="mb-3">
+          <label htmlFor="username" className="form-label">
+            User Name:
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="username"
+            placeholder="user..."
+            name="username"
+            {...register("username", {
+              required: {
+                value: true,
+                message: "Please enter a name",
+              },
+              minLength: {
+                value: 5,
+                message: "Please enter more than 5 characters",
+              },
+              maxLength: {
+                value: 25,
+                message: "Please enter less than 25 characters",
+              },
+            })}
+          />
+          <p className="text-center text-danger">{errors?.username?.message}</p>
+        </div>
 
-export default FormUser
+        <div className="mb-3">
+          <label htmlFor="adress" className="form-label">
+            Adress
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="adress"
+            placeholder="adress"
+            name="adress"
+          />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="numberPhone" className="form-label">
+            Phone Number:
+          </label>
+          <input
+            type="tel"
+            className="form-control"
+            id="numberPhone"
+            placeholder="numberPhone"
+            name="phone"
+          />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="exampleFormControlInput1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleFormControlInput1"
+            placeholder="name@example.com"
+            name="email"
+          />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="extraFact" className="form-label">
+            Extra fact
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="extraFact"
+            placeholder="extraFact"
+            name="extra"
+          />
+        </div>
+        <div className="d-flex justify-content-center align-content-center align-items-center">
+          <button
+            className="btn border-success text-success"
+            onClick={getDataForm}
+          >
+            Accept
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default FormUser;
